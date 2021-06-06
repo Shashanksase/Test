4ab80dbe95b1819f0309ab7a580f1335791542a5
shashank sase <39193503+Shashanksase@users.noreply.github.com>
2021-06-06 21:55:18 +0530

    1
    1

--- src/CBSEPercentage.java
+++ src/CBSEPercentage.java
@@ -5,11 +5,11 @@ public class CBSEPercentage {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.print("Enter you physic marks: ");
 		int physics = scan.nextInt();
 		int phy = physics;
-		if (phy<100) {
+		if (phy < 100) {
 			System.out.println("The value enter is below 100  ");
 		} else {
 			System.out.println("The value enter is above 100 ");
