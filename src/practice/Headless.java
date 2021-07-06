d7d043c92a6e414279092074a49935fb3619d2e7
shashank sase <Shashank.sase@gmail.com>
2021-07-06 11:17:11 +0530

    0607

--- src/practice/Headless.java
+++ src/practice/Headless.java
@@ -1,6 +1,5 @@
 package practice;
 
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
